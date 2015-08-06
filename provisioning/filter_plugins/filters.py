9105d4d62207b2420e9a507b01727ce595bafbf3
Sean 'Shaleh' Perry <shaleh@speakeasy.net>
2015-08-06 13:04:17 -0700

    Generate /etc/hosts. Add node details to facts.

---
+++ provisioning/filter_plugins/filters.py
@@ -0,0 +1,10 @@
+"""
+Needed because the jinja2 version is too old for
+selectattr(foo, equalto, value)
+"""
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'byattr': lambda lst, k, v: [i for i in lst if i[k] == v]
+        }
---
+++ tools/generate-host-file.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/ruby
+
+require 'fileutils'
+require 'ipaddr'
+require 'yaml'
+
+template_file = 'provisioning/roles/common/files/etchosts.in'
+etchosts_file = template_file[0 .. -4]
+nodes_file = 'nodes.yaml'
+
+last_dir = Dir.pwd
+while ! File.exists?(nodes_file)
+    Dir.chdir("..")
+    if last_dir == Dir.pwd
+        puts "Error: cannot find files"
+        exit 1
+    end
+    last_dir = Dir.pwd
+end
+
+if FileUtils.uptodate?(etchosts_file, [nodes_file, template_file])
+    exit 0
+end
+
+nodes = YAML.load_file(nodes_file)
+
+etchosts_template = File.read(template_file)
+
+hosts = nodes.map { |n| [ IPAddr::new(n["ip"]), n["hostname"] ] }.sort
+
+File.open(etchosts_file, "w") do |fp|
+    fp.write(etchosts_template)
+
+    hosts.each do |ip,name|
+        fp.write("%s\t%s\n" % [ip, name])
+    end
+end
